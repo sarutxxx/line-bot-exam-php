f0bfd3f64aea62cb9fc5f87311b5cc87dd667b8c
sarutxxx <44718524+sarutxxx@users.noreply.github.com>
2021-05-14 16:47:14 +0700

    ch comp

--- ss_bot.php
+++ ss_bot.php
@@ -21,59 +21,13 @@ if($inputtext == "สวัสดี") {
 } else if ($inputtext == "ทำอะไรได้บ้าง") {
 	$arrPostData['messages'][0]['type'] = 'text';
 	$arrPostData['messages'][0]['text'] = "มากมายค่ะ";
-  
-} else if ($inputtext == "เปิดไฟนอน1") {
-  	$mode = curl_init("http://128.199.137.43:3000/smtbot2017/mode/5/o");
-  	curl_exec($mode);
-  	$digital = curl_init("http://128.199.137.43:3000/smtbot2017/digital/5/1");
-  	curl_exec($digital);
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "เปิดไฟแล้วครับ";
-  
-} else if ($inputtext == "ปิดไฟนอน1") {
-  	$mode = curl_init("http://128.199.137.43:3000/smtbot2017/mode/5/o");
-  	curl_exec($mode);
-  	$digital = curl_init("http://128.199.137.43:3000/smtbot2017/digital/5/0");
-  	curl_exec($digital);
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "ปิดไฟแล้วครับ";
-
-} else if ($inputtext == "ความชื้น") {
-  	$s = file_get_contents("http://128.199.137.43:3000/smtbot2017/variable/humidity");
- 	 $h = json_decode($s, true);
-  	$hu = $h['humidity'];
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "ความชื้นสัมพัธน์ตอนนี้ " . $hu . "%";
-
-} else if ($inputtext == "อุณหภูมิ") {
-  	$s = file_get_contents("http://128.199.137.43:3000/smtbot2017/variable/temperature");
-  	$h = json_decode($s, true);
-  	$hu = $h['temperature'];
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "อุณหภูมิตอนนี้ " . $hu . " C";
-
-} else if ($inputtext == "อากาศ") {
-  	$s = file_get_contents("http://128.199.137.43:3000/smtbot2017/variable/temperature");
-  	$h = json_decode($s, true);
-  	$hu = $h['temperature'];
- 	$s2 = file_get_contents("http://128.199.137.43:3000/smtbot2017/variable/humidity");
- 	$h2 = json_decode($s2, true);
- 	$hu2 = $h2['humidity'];
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "อุณหภูมิ " . $hu . " C | ความชื้น " . $hu2 . " %";
-
-} else if ($inputtext == "แผนที่farm") {
-	$arrPostData['messages'][0]['type'] = "location";
-	$arrPostData['messages'][0]['title'] = "Serarom Farm";
-	$arrPostData['messages'][0]['address'] = "หมู่ 7 ตำบล บ้านแลง อำเภอเมืองระยอง ระยอง 21000";
-	$arrPostData['messages'][0]['latitude'] = "12.70362";
-	$arrPostData['messages'][0]['longitude'] = "101.3655878";
  
-} else if ($inputtext == "รายงาน") {
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "หลอดไฟ: นอน1-ปิด, นอน2-เปิด*, นอน3-ปิด | แอร์: นอน1-เปิด*, นอน2-เปิด*, นอน3-ปิด";
-
 } else if ($inputtext == "เยี่ยม") {
+	$arrPostData['messages'][0]['type'] = "sticker";
+	$arrPostData['messages'][0]['packageId'] = "1";
+	$arrPostData['messages'][0]['stickerId'] = "13";
+
+} else if ($inputtext == "Change Completed") {
 	$arrPostData['messages'][0]['type'] = "sticker";
 	$arrPostData['messages'][0]['packageId'] = "1";
 	$arrPostData['messages'][0]['stickerId'] = "13";
@@ -93,21 +47,12 @@ if($inputtext == "สวัสดี") {
 	$arrPostData['messages'][0]['originalContentUrl'] = "https://still-beach-54304.herokuapp.com/p3.jpg";
 	$arrPostData['messages'][0]['previewImageUrl'] = "https://still-beach-54304.herokuapp.com/p3.jpg";
 
-} else if ($inputtext == "เรียนที่ไหน") {
-	$arrPostData['messages'][0]['type'] = 'text';
-	$arrPostData['messages'][0]['text'] = "https://www.facebook.com/RSU.IT.SMT";
 	
 }else{
  	$arrPostData['messages'][0]['type'] = 'text';
  	$arrPostData['messages'][0]['text'] = "ไม่เข้าใจคำสั่งค่ะ";
 }
 
-if ($w[0] == "อากาศ" and isset($w[1])) {
-	$prov = $w[1];
-  	$a = file_get_contents("http://m.smart-fttx.com/test-weather.php?prov=$prov&token=inb32XpbrlLgd8HMCzhbhZsJq7VxkqqA");
- 	$arrPostData['messages'][0]['type'] = 'text';
- 	$arrPostData['messages'][0]['text'] = $a;
-}
 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
